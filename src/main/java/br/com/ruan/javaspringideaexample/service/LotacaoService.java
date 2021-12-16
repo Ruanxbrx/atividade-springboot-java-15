33b8f6f11b8195af7cefe6cb41438d0a96b3ff34
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 12:09:20 -0300

    correcao na classe LotacaoService

--- src/main/java/br/com/ruan/javaspringideaexample/service/LotacaoService.java
+++ src/main/java/br/com/ruan/javaspringideaexample/service/LotacaoService.java
@@ -1,11 +1,13 @@
 package br.com.ruan.javaspringideaexample.service;
 
 import br.com.ruan.javaspringideaexample.entities.Lotacao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import br.com.ruan.javaspringideaexample.repository.LotacaoRepository;
 
 @Service
 public class LotacaoService {
+    @Autowired
     private final LotacaoRepository repository;
 
     public LotacaoService(LotacaoRepository repository) {
@@ -16,7 +18,8 @@ public class LotacaoService {
         return lotacaoSalva;
     }
     public Lotacao buscaPorID(Long id){
-        Lotacao lotacaoBuscada = repository.getById();
+        Lotacao lotacaoBuscada = repository.getById(id);
         return lotacaoBuscada;
     }
+
 }
