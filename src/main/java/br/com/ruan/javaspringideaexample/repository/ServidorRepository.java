54a5693739bc16f75cb869887c543e87ebdf9fbf
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 14:57:32 -0300

    Creando consulta de Servidor por matricula

--- src/main/java/br/com/ruan/javaspringideaexample/repository/ServidorRepository.java
+++ src/main/java/br/com/ruan/javaspringideaexample/repository/ServidorRepository.java
@@ -4,7 +4,9 @@ import br.com.ruan.javaspringideaexample.entities.Servidor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ServidorRepository extends JpaRepository<Servidor,Long> {
-
+    Optional<Servidor> findByMatricula(String matricula);
 }
