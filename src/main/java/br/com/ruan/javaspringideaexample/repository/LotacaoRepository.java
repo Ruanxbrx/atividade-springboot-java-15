0a8ee557382d0a73013568f3c73d8f86530a5ca5
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 14:05:42 -0300

    Creando consulta de lotacao por descreição

--- src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
+++ src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
@@ -1,9 +1,15 @@
 package br.com.ruan.javaspringideaexample.repository;
 
 import br.com.ruan.javaspringideaexample.entities.Lotacao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LotacaoRepository extends JpaRepository<Lotacao,Long> {
+    Optional<Lotacao> findByDescricao(String descricao);
 }
