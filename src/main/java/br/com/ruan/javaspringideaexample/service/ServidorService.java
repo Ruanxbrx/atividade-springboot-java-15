4c9a2fe9ee06b9c5eb9bf32ffaa95aaee2432e51
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 14:59:15 -0300

    Creando consulta de Servidor por matricula

--- src/main/java/br/com/ruan/javaspringideaexample/service/ServidorService.java
+++ src/main/java/br/com/ruan/javaspringideaexample/service/ServidorService.java
@@ -1,4 +1,28 @@
 package br.com.ruan.javaspringideaexample.service;
 
+import br.com.ruan.javaspringideaexample.entities.Servidor;
+import br.com.ruan.javaspringideaexample.repository.ServidorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class ServidorService {
+
+    @Autowired
+    private final ServidorRepository repository;
+
+    public ServidorService(ServidorRepository repository){
+        this.repository = repository;
+    }
+    public Servidor salvar(Servidor novoServidor){
+        Servidor servidorSalva = repository.save(novoServidor);
+        return servidorSalva;
+    }
+    public Servidor buscaPorMatricula(String matricula){
+        Optional<Servidor> servidorBuscada =repository.findByMatricula(matricula);
+        return servidorBuscada.get();
+    }
+
 }
