5d611c7761a82facc4559c57d4db9477d63d8774
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 14:58:29 -0300

    Creando consulta de Servidor por matricula

--- src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
+++ src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
@@ -1,12 +1,9 @@
 package br.com.ruan.javaspringideaexample.repository;
 
 import br.com.ruan.javaspringideaexample.entities.Lotacao;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
