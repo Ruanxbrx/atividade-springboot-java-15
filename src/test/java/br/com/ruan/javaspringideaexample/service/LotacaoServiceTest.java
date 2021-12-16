8e3be671c7616b51fdacabb96371bf465623db56
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 14:58:10 -0300

    Creando consulta de Servidor por matricula

--- src/test/java/br/com/ruan/javaspringideaexample/service/LotacaoServiceTest.java
+++ src/test/java/br/com/ruan/javaspringideaexample/service/LotacaoServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDateTime;
 
@@ -42,6 +40,7 @@ public class LotacaoServiceTest {
         Assertions.assertNotNull(buscaLotacao.getId());
 
     }
+
 
 
 }
