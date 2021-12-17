8b1d6b7f5ae3bb21d47328898a05a5a3449039f0
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-17 13:52:48 -0300

    Creando teste unitario ServidorService

--- src/test/java/br/com/ruan/javaspringideaexample/service/ServidorServiceTest.java
+++ src/test/java/br/com/ruan/javaspringideaexample/service/ServidorServiceTest.java
@@ -2,27 +2,41 @@ package br.com.ruan.javaspringideaexample.service;
 
 import br.com.ruan.javaspringideaexample.controller.dto.ServidorCadastroDto;
 import br.com.ruan.javaspringideaexample.entities.Servidor;
+import br.com.ruan.javaspringideaexample.repository.ServidorRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
 public class ServidorServiceTest {
 
-    @Autowired
+    @InjectMocks
     private ServidorService servidorService;
+
+    @Mock
+    private ServidorRepository repository;
 
     @Test
     @DisplayName("deve salvar um novo Servidor")
     void deveSalvarUmNovoServidor() {
         //cenario
         ServidorCadastroDto dto = new ServidorCadastroDto("bbbb",null);
+        Servidor servidor = new Servidor(1L,"bbbb","Mat1",LocalDateTime.now(),null );
+        when(repository.save(any())).thenReturn(servidor);
         //execucao
         Servidor servidorSalva = servidorService.salvar(dto);
         //verificacao
-        Assertions.assertNotNull(servidorSalva.getId());
+        assertEquals(servidorSalva, servidor);
     }
 
     @Test
@@ -30,10 +44,13 @@ public class ServidorServiceTest {
     void deveRetornarUmNovoServidor() {
         //cenario
         ServidorCadastroDto dto = new ServidorCadastroDto("bbbb",null);
+        Servidor servidor = new Servidor(1L,"bbbb","Mat1",LocalDateTime.now(),null );
+        when(repository.save(any())).thenReturn(servidor);
+        when(repository.findByMatricula("Mat1")).thenReturn(java.util.Optional.of(servidor));
         Servidor servidorSalva = servidorService.salvar(dto);
         //execucao
         Servidor buscaServidor = servidorService.buscaPorMatricula(servidorSalva.getMatricula());
         //verificacao
-        Assertions.assertNotNull(buscaServidor.getId());
+        assertEquals(buscaServidor, servidor);
     }
 }
