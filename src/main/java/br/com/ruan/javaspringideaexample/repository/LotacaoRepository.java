828b80dfa5779c6874f02efd8d761dfcab316bc7
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 22:31:18 -0300

    adicionei função findALLById

--- src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
+++ src/main/java/br/com/ruan/javaspringideaexample/repository/LotacaoRepository.java
@@ -4,9 +4,13 @@ import br.com.ruan.javaspringideaexample.entities.Lotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LotacaoRepository extends JpaRepository<Lotacao,Long> {
+
     Optional<Lotacao> findByDescricao(String descricao);
+
+    List<Lotacao> findAllById(Iterable<Long> longs);
 }
