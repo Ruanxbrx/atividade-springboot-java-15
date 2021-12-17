439f79c1f3eaedce2e32262338032389f1341cc3
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 22:38:29 -0300

    refatorado metado de salvar servidor

--- src/main/java/br/com/ruan/javaspringideaexample/service/ServidorService.java
+++ src/main/java/br/com/ruan/javaspringideaexample/service/ServidorService.java
@@ -1,24 +1,45 @@
 package br.com.ruan.javaspringideaexample.service;
 
+import br.com.ruan.javaspringideaexample.controller.dto.ServidorCadastroDto;
+import br.com.ruan.javaspringideaexample.entities.Lotacao;
 import br.com.ruan.javaspringideaexample.entities.Servidor;
 import br.com.ruan.javaspringideaexample.repository.ServidorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ServidorService {
 
-    @Autowired
-    private final ServidorRepository repository;
+    final static String PREFIXO_MATRICULA = "MAT";
 
-    public ServidorService(ServidorRepository repository){
+    private final ServidorRepository repository;
+    private final LotacaoService lotacaoService;
+
+    public ServidorService(ServidorRepository repository, LotacaoService lotacaoService) {
         this.repository = repository;
+        this.lotacaoService = lotacaoService;
     }
-    public Servidor salvar(Servidor novoServidor){
-        Servidor servidorSalva = repository.save(novoServidor);
-        return servidorSalva;
+
+    @Transactional
+    public Servidor salvar(ServidorCadastroDto servidorDto){
+
+        Servidor servidor;
+
+        if (servidorDto.getLotacoes() != null) {
+            Set<Lotacao> lotacoes = new HashSet<>(lotacaoService.buscarPorTodosIds(servidorDto.getLotacoes()));
+            servidor = new Servidor(servidorDto.getName(), LocalDateTime.now(), lotacoes);
+        } else {
+            servidor = new Servidor(servidorDto.getName(), LocalDateTime.now());
+        }
+
+        repository.save(servidor);
+        servidor.setMatricula(PREFIXO_MATRICULA + servidor.getId());
+        return repository.save(servidor);
     }
     public Servidor buscaPorMatricula(String matricula){
         Optional<Servidor> servidorBuscada = repository.findByMatricula(matricula);
