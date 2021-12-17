7ce47cda371a2e78e40af8e14f062536d3524e27
Ruan Freitas <ruan10d5@hoymail.com>
2021-12-16 22:39:13 -0300

    refatorado metado de salvar lotacao

--- src/main/java/br/com/ruan/javaspringideaexample/service/LotacaoService.java
+++ src/main/java/br/com/ruan/javaspringideaexample/service/LotacaoService.java
@@ -1,10 +1,13 @@
 package br.com.ruan.javaspringideaexample.service;
 
+import br.com.ruan.javaspringideaexample.controller.dto.LotacaoDto;
 import br.com.ruan.javaspringideaexample.entities.Lotacao;
 import br.com.ruan.javaspringideaexample.repository.LotacaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,13 +19,18 @@ public class LotacaoService {
     public LotacaoService(LotacaoRepository repository) {
         this.repository = repository;
     }
-    public Lotacao salvar(Lotacao novaLotacao){
-        Lotacao lotacaoSalva = repository.save(novaLotacao);
-        return lotacaoSalva;
+
+    public Lotacao salvar(LotacaoDto lotacaoDto){
+        Lotacao lotacao = new Lotacao(lotacaoDto.getDescricao(), LocalDateTime.now(),lotacaoDto.getTipoLotacao());
+        return repository.save(lotacao);
     }
     public Lotacao buscaPorDescricao(String descricao){
         Optional<Lotacao> lotacaoBuscada =repository.findByDescricao(descricao);
         return lotacaoBuscada.get();
+    }
+
+    public List<Lotacao> buscarPorTodosIds(List<Long> ids){
+        return repository.findAllById(ids);
     }
 
 }
